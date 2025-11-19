9dec3e17b7bf6fd568a3fc8d991aca488c64a4aa
Ty Davis <tydavisxc2019@gmail.com>
2025-11-19 16:28:26 -0700

    Got the asynchronous scaffolding implemented

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import sys
-from matplotlib.tri import Triangulation
 from PyQt6 import QtWidgets, uic
 from PyQt6.QtGui import QShortcut, QKeySequence
 import plotting
@@ -10,6 +9,8 @@ from motor_connection import MotorConnection
 from serial.serialutil import SerialException
 from scans import all_scans, AbstractScan
 from datetime import datetime
+import asyncio
+import qasync
 
 class MyMainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -21,6 +22,8 @@ class MyMainWindow(QtWidgets.QMainWindow):
 
         # load the parameters
         self.params_man = ParamsManager()
+
+        self.logfile = None
 
         # configure the motor connection
         self.motor_conn = None
@@ -38,6 +41,7 @@ class MyMainWindow(QtWidgets.QMainWindow):
         self.move_with_arrows_button.clicked.connect(self.actionMove_with_Arrow_Keys.trigger)
         self.view_params_b.clicked.connect(self.actionView_Parameters.trigger)
         self.load_params_b.clicked.connect(self.load_parameters)
+
         self.start_button.clicked.connect(self.start_test)
         self.cancel_button.clicked.connect(self.cancel_test)
 
@@ -50,19 +54,48 @@ class MyMainWindow(QtWidgets.QMainWindow):
     def edit_parameters(self):
         ...
 
+
     def log(self, *args):
         self.status_label.setText(' '.join(args))
         print(datetime.now(), *args)
 
-    def start_test(self):
+    # def start_test_sync(self):
+    #     try:
+    #         loop = asyncio.get_event_loop()
+    #     except RuntimeError:
+    #         loop = asyncio.new_event_loop()
+    #         asyncio.set_event_loop(loop)
+    #
+    #     loop.run_until_complete(self.start_test())
+
+    @qasync.asyncSlot()
+    async def start_test(self):
         scan: AbstractScan = self.select_scan.currentData()
+        print("HERE")
+        scan.reset_cancel()
+
         scan.populate_state(self.motor_conn, self.params_man.params, self.log)
 
-        # toggle start/cancel button and disable a bunch of stuff
-        self.start_button.hide()
-        self.cancel_button.show()
+        try:
+            self.start_button.hide()
+            self.cancel_button.show()
+            self.log("Starting scan...")
+
+            results = await scan.run_procedure()
+            # toggle start/cancel button and disable a bunch of stuff
+
+            return results
+        except asyncio.CancelledError:
+            self.log("Scan cancelled by user")
+        finally:
+            self.cancel_button.hide()
+            self.start_button.show()
+
 
     def cancel_test(self):
+        scan = self.select_scan.currentData()
+        scan.cancel()
+
         self.cancel_button.hide()
         self.start_button.show()
 
@@ -156,6 +189,12 @@ class ViewParamsDialog(QtWidgets.QDialog):
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
+
+    loop = qasync.QEventLoop(app)
+    asyncio.set_event_loop(loop)
+
     window = MyMainWindow()
     window.show()
-    sys.exit(app.exec())
+    with loop:
+        loop.run_forever()
+    # sys.exit(app.exec())
--- scans.py
+++ scans.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from motor_connection import MotorConnection
 from typing import List, Tuple, Dict, Callable
+import asyncio
 
 class AbstractScan(ABC):
     def __init__(self):
@@ -9,9 +10,10 @@ class AbstractScan(ABC):
         self.motor_conn: MotorConnection
         self.params: Dict
         self.log: Callable
+        self._cancel_event: asyncio.Event = asyncio.Event()
 
     @abstractmethod
-    def run_procedure(self) -> List[Tuple[float, float, float, float]]:
+    async def run_procedure(self) -> List[Tuple[float, float, float, float]]:
         pass
 
     def populate_state(self, motor_conn, params, log):
@@ -19,31 +21,50 @@ class AbstractScan(ABC):
         self.params = params
         self.log = log
 
+    def cancel(self):
+        self._cancel_event.set()
+
+    def reset_cancel(self):
+        self._cancel_event.clear()
+
+    async def _check_cancelled(self):
+        if self._cancel_event.is_set():
+            raise asyncio.CancelledError("Scan cancelled by user")
+
     def __str__(self):
         return f"{self.name} - {self.instructions}"
 
 class ThreeDFastScan(AbstractScan):
     def __init__(self):
+        super().__init__()
         self.name = "3D Fast Scan"
         self.instructions = """Transmit Antenna polarity should be vertical. Set AUT to φ = 90 and θ = 0"""
 
-    def run_procedure(self):
-        return [(0., 0., 0., 0.)]
+    async def run_procedure(self):
+        self.log("IS THIS WORKING?")
+        results = []
+        for i in range(100):
+            await self._check_cancelled()
+            await asyncio.sleep(0.1)
+            results.append((float(i), 0., 0., 0.))
+        return results
 
 class ThetaScan(AbstractScan):
     def __init__(self):
+        super().__init__()
         self.name = "2D Scan θ"
         self.instructions = """Here are the instructions"""
 
-    def run_procedure(self):
+    async def run_procedure(self):
         return [(0., 0., 0., 0.)]
 
 class PhiScan(AbstractScan):
     def __init__(self):
+        super().__init__()
         self.name = "2D Scan φ"
         self.instructions = """Here are the instructions"""
 
-    def run_procedure(self):
+    async def run_procedure(self):
         return [(0., 0., 0., 0.)]
 
 
